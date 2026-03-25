939adb90d80f893abcbb3e359299bd7b5be05651
Hicham Ouazik <hichamouazik31@gmail.com>
2026-03-25 17:37:39 +0100

    Update Ex4.java

--- TP1/src/partie1/Ex4.java
+++ TP1/src/partie1/Ex4.java
@@ -1,15 +1,16 @@
 package partie1;
-
+import java.util.Scanner;
 public class Ex4 {
-
-	public static void main(String[] args) {
-		int i ;
-		i=0;
-		while ( i <=10) {
-			
-			System.out.println("Le plus grand valeur : " + i);
-			i++;
-		}
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Entrez N : ");
+        int n = sc.nextInt();
+        int somme = 0, i = 1;
+        while (i <= n) {
+            somme += i;
+            i++;
+        }
+        System.out.println("Somme des " + n + " premiers entiers = " + somme);
+    }
 }
+
