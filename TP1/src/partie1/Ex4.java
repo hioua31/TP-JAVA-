d2e0ef7fd256f44fcec5ae259417bcf6476d6049
Hcham OUAZIK <hichamouazik31@gmail.com>
2026-04-10 22:35:27 +0200

    tp1

--- TP1/src/partie1/Ex4.java
+++ TP1/src/partie1/Ex4.java
@@ -1,33 +1,24 @@
 package partie1;
+
 import java.util.Scanner;
+
 public class Ex4 {
-<<<<<<< Updated upstream
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Entrez N : ");
         int n = sc.nextInt();
+
         int somme = 0, i = 1;
+
         while (i <= n) {
             somme += i;
             i++;
         }
+
         System.out.println("Somme des " + n + " premiers entiers = " + somme);
+
+        sc.close();
     }
 }
-=======
-
-	    public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        System.out.print("Entrez N : ");
-	        int n = sc.nextInt();
-	        int somme = 0, i = 1;
-	        while (i <= n) {
-	            somme += i;
-	            i++;
-	        }
-	        System.out.println("Somme des " + n + " premiers entiers = " + somme);
-	        sc.close();
-	    }
-	}
->>>>>>> Stashed changes
-
